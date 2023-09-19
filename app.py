c837b9df0728e609def6327dc973f8a094852c31
CindyChang0604 <hebe860604@gmail.com>
2023-09-19 14:59:25 +0800

    update time

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def submit_attendance():
         
         # 根據時間是上午還是下午添加上午/下午，並將時間格式化
         if 打卡時間.hour < 12:
-            formatted_time = 打卡時間.strftime('%Y/%m/%d 上午 %I:%M:%S')
+            打卡時間 = 打卡時間.strftime('%Y/%m/%d 上午 %I:%M:%S')
         else:
-            formatted_time = 打卡時間.strftime('%Y/%m/%d 下午 %I:%M:%S')
+            打卡時間 = 打卡時間.strftime('%Y/%m/%d 下午 %I:%M:%S')
         
         
         if 員工姓名列表:
